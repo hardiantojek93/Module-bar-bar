c29978c3a30c047028e5353dc67166c5599d0732
namae hardianto desu <71861395+hardiantojek93@users.noreply.github.com>
2021-01-01 11:02:43 +0000

    Add files via upload
    tinggal hapus msghanfler

---
+++ index.js
@@ -0,0 +1,54 @@
+const { create, Client } = require('@open-wa/wa-automate')
+ const left = require('./lib/left')
+const welcome = require('./lib/welcome')
+//const anto = require('./anto')
+const msgHandler = require('./anto')
+const options = require('./options')
+const start = async (anto = new Client()) => {
+        console.log('[SERVER] Server Started!')
+        // Force it to keep the current session
+        anto.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') anto.forceRefocus()
+        })
+        // listening on message
+        anto.onMessage((async (message) => {
+            anto.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    anto.cutMsgCache()
+                }
+            })
+           msgHandler(anto, message)
+            //require('./anto.js')(anto, message)
+        }))
+
+        anto.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(anto, heuh)
+            left(anto, heuh)
+            }))
+        
+        anto.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
+            if (totalMem < 30) { 
+            	anto.sendText(chat.id, `Anooo neee anggota Grub Cuma  ${totalMem}, Invite Paimom Di lain Hari ya kak `).then(() => anto.leaveGroup(chat.id)).then(() => clantoeleteChat(chat.id))
+            } else {
+                anto.sendText(chat.groupMetadata.id, `Halo warga grup *${chat.contact.name}* terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *!help*`)
+            }
+        }))
+
+        /*anto.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) anto.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        anto.onIncomingCall(( async (call) => {
+            await anto.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!')
+            .then(() => anto.contactBlock(call.peerJid))
+        }))
+    }
+
+create(options(true, start))
+    .then(anto => start(anto))
+    .catch((error) => console.log(error))
